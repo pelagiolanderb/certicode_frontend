60744e5641a97541925345b724e32056fe2e9f03
mbhornido <angelohornido2@gmail.com>
2025-03-13 10:13:00 +0800

    dashboard

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -16,7 +16,10 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
+          <StatisticsChart />
           <RecentOrders />
+
+
           {/* <MonthlySalesChart /> */}
         </div>
 
