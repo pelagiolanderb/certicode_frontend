d3f2b3b2b178c66be2de90526c98a9cc1f402e01
mbhornido <angelohornido2@gmail.com>
2025-03-22 21:53:34 +0800

    update_hello

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <AppWrapper>
+      <AppWrapper> 
         <App />
       </AppWrapper>
     </ThemeProvider>
